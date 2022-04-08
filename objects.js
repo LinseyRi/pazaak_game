6a7d2c07d0787d0715c8c33285d67de2b910cbe4
Linsey <linseyrimmer@hotmail.co.uk>
2022-04-08 07:17:23 +0100

    Initial Commit

---
+++ objects.js
@@ -0,0 +1,201 @@
+// ------> image dicts 
+playerCardImages = {
+    '1': 'assets/plus_1.png',
+    '2': 'assets/plus_2.png',
+    '3': 'assets/plus_3.png',
+    '4': 'assets/plus_4.png',
+    '-1': 'assets/minus_1.png',
+    '-2': 'assets/minus_2.png',
+    '-3': 'assets/minus_3.png', 
+    '-4': 'assets/minus_4.png'
+}
+
+houseCardImages = {
+    '1': 'assets/house_1.png',
+    '2': 'assets/house_2.png',
+    '3': 'assets/house_3.png',
+    '4': 'assets/house_4.png',
+    '5': 'assets/house_5.png',
+    '6': 'assets/house_6.png',
+    '7': 'assets/house_7.png',
+    '8': 'assets/house_8.png',
+    '9': 'assets/house_9.png',
+    '10': 'assets/house_10.png',
+}
+
+// ------> Helper Object Creation <----------
+
+// Create Card Object
+class Card {
+    constructor(value) {
+        this.value = value; 
+    }
+
+    show() {
+        console.log(`Card Value: ${this.value}`);
+    }
+
+    renderOnBoard(gameBoard, gameBoardId) {
+        let addedCard = document.createElement('div');
+        addedCard.classList.add('card-slot');
+        addedCard.value = this.value; 
+        addedCard.innerText = this.value; 
+        document.getElementById(gameBoardId).appendChild(addedCard);
+        gameBoard.cards.push(this.card); 
+    }
+}
+
+class MainDeck {
+    constructor() {
+        this.deck = new Array(); 
+        this.buildDeck(); 
+    }
+
+    shuffle() {
+        for (let i = 0; i != this.deck.length; i++) {
+            let r = Math.floor(Math.random() * i) + 1; 
+            let tempCard = this.deck[i];
+            this.deck[i] = this.deck[r]
+            this.deck[r] = tempCard; 
+        }
+    }
+
+    buildDeck() {
+        for (let i = 1; i <= 10; i++) {
+            for (let x = 1; x <= 4; x++) {
+                let temp = new Card(i);
+                this.deck.push(temp); 
+            }
+        }
+        this.shuffle(); 
+    }
+
+    show() {
+        for (let card of this.deck) {
+            card.show()
+        }
+    }
+
+    drawCard() {
+        return this.deck.pop();
+    }
+
+    renderCard(gameBoard, boardElementId, totalSpanId) {
+        let card = this.drawCard(); 
+        gameBoard.cardLaid(card, boardElementId, totalSpanId, true);
+    }
+}
+
+class PlayerDeck extends MainDeck {
+
+    buildDeck() {
+        let cardOptions = [-1, -2, -3, -4, 1, 2, 3, 4];
+        for (let i = 1; i <= 10; i++) {
+            let r = Math.floor(Math.random() * 7) + 1;
+            let newCard = new Card(cardOptions[r]);
+            this.deck.push(newCard); 
+        }
+    }
+}
+
+class Player {
+    constructor(playerName) {
+        this.name = playerName; 
+        this.playerDeck = new PlayerDeck(); 
+        this.hand = new Array(); 
+        this.createHand(); 
+    }
+
+    createHand() {
+        for (let i = 1; i <= 4; i++) {
+            this.hand.push(this.playerDeck.deck.pop()); 
+        }
+    }
+
+    showHand() {
+        for (let card of this.hand) {
+            card.show(); 
+        }
+    }
+
+    renderHand(handElementId) {
+        this.hand.forEach(function (card) {
+            let newCardElement = document.createElement('img'); 
+            let imageSrc = playerCardImages[card.value.toString()];
+            newCardElement.src = imageSrc;
+            newCardElement.classList.add('hand-card'); 
+            newCardElement.value = card.value; 
+            newCardElement.onclick = function (e) {
+                let newBoardCard = new Card(parseInt(e.target.value)); 
+                GAME_BOARD.cardLaid(newBoardCard, "game-board", "board-total", false);
+                e.target.parentNode.removeChild(e.target);
+            };
+            document.getElementById(handElementId).appendChild(newCardElement); 
+        })    
+    }
+
+    playCard(card, gameBoardId) {
+        // TODO implement ability to play card from hand 
+    }
+}
+
+class GameBoard {
+    constructor() {
+        this.cards = new Array(); 
+        this.total = this.totalCalculator(this.cards); 
+    }
+
+    totalCalculator(currentCards) {
+        let total = 0; 
+        for (let card of currentCards) {
+            total = total + card.value; 
+        }
+        return total; 
+    }
+
+    cardLaid(card, gameBoardId, totalSpanId, house) {
+        console.log(house);
+        this.cards.push(card); 
+        let newCardElement = document.createElement('img');
+        if (house == true) {
+            var imageSrc = houseCardImages[card.value.toString()];
+        } else {
+            var imageSrc = playerCardImages[card.value.toString()];
+        }
+        newCardElement.src = imageSrc;
+        newCardElement.classList.add('board-card');
+        newCardElement.value = card.value;
+        document.getElementById(gameBoardId).appendChild(newCardElement);
+        this.updateTotal(totalSpanId);
+    }
+
+    testWin() {
+        if ( this.total == 20 ) {
+            return 'win'; 
+        } else if ( this.total > 20 ) {
+            return 'lose'; 
+        } else if ( this.total < 20 ) {
+            return 'continue';
+        }
+    }
+
+    updateTotal(totalSpanId) {
+        let totalTracker = document.getElementById(totalSpanId);
+        let runningTotal = this.totalCalculator(this.cards); 
+        totalTracker.innerText = runningTotal; 
+    }
+}
+
+// ------> Game Content <----------
+
+const GAME_BOARD = new GameBoard(); 
+const MAIN_DECK = new MainDeck(); 
+var playerOne = new Player("Player One"); 
+
+// ---- > Game Manager Functions
+function gameManager(player) {
+    player.renderHand("player-hand");
+    MAIN_DECK.renderCard(GAME_BOARD, "game-board", "board-total");
+}
+
+gameManager(playerOne);
